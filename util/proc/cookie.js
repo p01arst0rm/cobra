44f7c53e6987cb455a9095a246f74c78cbac9b50
p01arst0rm <polar@ever3st.com>
2019-04-25 18:46:17 +0100

    improvements to proc/cookie

--- util/proc/cookie.js
+++ util/proc/cookie.js
@@ -3,31 +3,19 @@
 
 const expire = '=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;'
 
-function formatcookie() {
-  const decodedCookie = decodeURIComponent(document.cookie)
-  const cookieArray = decodedCookie.split(';')
-  const keypairArray = []
+export function getCookie(cname) {
+  const cookieArray = decodeURIComponent(document.cookie).split(';')
 
   for (let i = 0; i < cookieArray.length; i += 1) {
-    const c = cookieArray[i].trim()
-
     // break up keypairs ( '=' is valid value inside KEY='VALUE')
-    const keypair = c.match(/((?:'|")[^*]*(?:'|"))|[^=]+/g)
-    keypairArray[i] = keypair
-  }
-  return keypairArray
-}
+    const keypair = cookieArray[i]
+      .trim().match(/((?:'|")[^*]*(?:'|"))|[^=]+/g)
 
-export function getCookie(cname) {
-  const keypairArray = formatcookie()
-  console.log(keypairArray)
-
-  for (let i = 0; i < keypairArray.length; i += 1) {
-    const keypair = keypairArray[i]
     if (keypair[0] === cname) {
       return keypair[1]
     }
   }
+  return null
 }
 
 export function setCookie(cname, value) {
