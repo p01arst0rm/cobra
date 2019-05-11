5d4f6f2906c2c487fdd9a1cdccc8a6df1bba1d71
p01arst0rm <polar@ever3st.com>
2019-05-11 21:39:19 +0100

    improvements to nav

--- components/App/NavLinks/Index.vue
+++ components/App/NavLinks/Index.vue
@@ -3,30 +3,35 @@
     <nuxt-link
       class="nav-link"
       to="/"
+      exact
     >
       Home
     </nuxt-link>
     <nuxt-link
       class="nav-link"
       to="/account/login"
+      exact
     >
       Login
     </nuxt-link>
     <nuxt-link
       class="nav-link"
       to="/account/signup"
+      exact
     >
       Signup
     </nuxt-link>
     <nuxt-link
       class="nav-link"
       to="/account/profile"
+      exact
     >
       Profile
     </nuxt-link>
     <nuxt-link
       class="nav-link"
       to="/about"
+      exact
     >
       About
     </nuxt-link>
