38929550743c18e77df9725700f90917daa2855c
p01arst0rm <dev@ever3st.com>
2026-04-24 13:01:12 +0100

    update confiig

---
+++ nuxt.config.ts
@@ -0,0 +1,33 @@
+import { defineNuxtConfig } from 'nuxt/config'
+import path from 'path'
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  pages: true,
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/test-utils',
+    '@pinia/nuxt'
+  ],
+  plugins: [
+	  { 
+      src: '~/plugins/velocity.js'
+    }],
+  // alias: {
+  //   '~': path.resolve(process.cwd(), 'app'),
+  // },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        stylus: {
+          // Use absolute path for reliability
+          paths: [path.resolve(process.cwd(), 'app')],
+          // Fix: Ensure define is an object and paths are absolute
+          define: {
+            '$config': path.resolve(process.cwd(), 'app/util/config.json')
+          }
+        }
+      }
+    }
+  }
+})
